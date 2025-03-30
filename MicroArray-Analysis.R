4e069594cdc9533a2c089a714ece94ef9106f3b9
Ky-Mani Miller <156713196+KMiller06@users.noreply.github.com>
2025-03-30 19:05:26 -0400

    Add files via upload

---
+++ MicroArray-Analysis.R
@@ -0,0 +1,8 @@
+#Author:Miller;Date:3/27/2025; Purpose: Gene Expression analysis using microarray
+
+
+#"C:\Users\Ky-Mani Miller\Downloads\GSE272045_RAW"
+
+
+#Set the working directory
+setwd("C:\Users\Ky-Mani Miller\Downloads\GSE272045_RAW")
