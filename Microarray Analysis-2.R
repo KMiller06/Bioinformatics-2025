d26cd839159380267266f2f0d10392512b7054bb
Ky-Mani Miller <156713196+KMiller06@users.noreply.github.com>
2025-04-10 11:47:18 -0400

    Add files via upload

--- Microarray Analysis-2.R
+++ Microarray Analysis-2.R
@@ -4,8 +4,34 @@
 library(affy)
 
 # Set the working directory
-setwd("C:\Users\Ky-Mani Miller\Downloads\GSE272045_RAW")
+setwd("C:/Real Data")
 
 #Read the CEL dataset from data folder
 
 data<- ReadAffy()
+
+#Boxplot the raw datasets
+boxplot(data)
+
+#Preprocess the microarray dataset by performing Normalization (by using function rma)
+norm_data<-rma(data)
+
+#Extract expression values
+final<-exprs(norm_data
+
+#Plot the normalized dataset
+boxplot(final)
+
+#save as normalized-plot
+
+
+#Classify treatment and control groups
+Treatment<-final[,1:5]
+Control<-final[,6:10]
+
+head(treatment)
+
+,before is column
+,after is rows
+
+
